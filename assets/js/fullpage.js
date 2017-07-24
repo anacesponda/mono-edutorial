ea137fa72e1c6e5508487b961a64ee456a211e4f
Ana Clara <anacesponda@gmail.com>
2017-07-24 03:41:03 -0300

    Versioón 2

---
+++ assets/js/fullpage.js
@@ -0,0 +1,8 @@
+// $(document).ready(function() {
+//     $('#fullpage').fullpage({
+//         slidesNavigation: true,
+//         scrollingSpeed: 200,
+//         normalScrollElements: '.footer-above',
+//         scrollOverflow: true
+//     });
+// });
